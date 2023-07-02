c39233477c58bdee636939d3515568b90eceafe1
Mark Nosal <mark.c.nosal@gmail.com>
2023-07-02 13:41:55 -0500

    redid lab and made code much better

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     if length == 0:
+#         print([])
+#     elif length == 1:
+#         print([0])
+#     else:
+#         fib_list = [0,1]
+
+#         while len(fib_list) < length:
+#             next_int = fib_list[-1] + fib_list[-2]
+#             fib_list.append(next_int)
+
+#         print(fib_list)
+
+
+# Much better code:
 def print_fibonacci(length):
     if length == 0:
-        print([])
+        fib_list = []
     elif length == 1:
-        print([0])
+        fib_list = [0]
     else:
         fib_list = [0,1]
-
-        while len(fib_list) < length:
-            next_int = fib_list[-1] + fib_list[-2]
-            fib_list.append(next_int)
-
-        print(fib_list)
+        for _ in range(length - 2):
+            fib_list.append(sum(fib_list[-2:]))
+    print(fib_list)
